f119f0525b54bf9e7b6d11be35037debfe26ad6c
Adam Lane <enalmada@gmail.com>
2025-01-30 17:43:06 -0800

    lockfile testing

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/start/config";
 import { config } from "dotenv";
 import { cloudflare } from "unenv";
-import viteRollbar from "vite-plugin-rollbar";
+// import viteRollbar from "vite-plugin-rollbar";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { cspRules } from "./app/lib/security/cspRules";
 import { generateSecurityHeaders } from "./app/lib/security/generate";
@@ -28,7 +28,7 @@ export default defineConfig({
 			}),
 			tailwindcss(),
 			// Upload source maps to Rollbar after build
-			...(process.env.ROLLBAR_SERVER_TOKEN
+			/* 			...(process.env.ROLLBAR_SERVER_TOKEN
 				? [
 						viteRollbar({
 							accessToken: process.env.ROLLBAR_SERVER_TOKEN,
@@ -38,7 +38,7 @@ export default defineConfig({
 							silent: false,
 						}),
 					]
-				: []),
+				: []), */
 			// TODO - serwist having trouble on cloudflare pages
 			// "~": process.env.CF_PAGES ? "/opt/buildhome/repo/app"  : "./app",
 			/*
@@ -161,6 +161,21 @@ export default defineConfig({
 					// In production, source maps are uploaded to Rollbar
 					sourcemap: process.env.NODE_ENV === "development",
 				},
+				/* 				external: [
+					// Mark server-only packages as external to exclude from client bundle
+					"better-auth",
+					"better-auth/adapters/*",
+					"better-auth/server",
+					"better-auth/dist/server",
+					"drizzle-orm",
+					"drizzle-orm/pg-core",
+					"drizzle-valibot",
+					"@neondatabase/serverless",
+					// Node.js built-in modules
+					"node:stream",
+					"node:stream/web",
+					"node:async_hooks",
+				], */
 			},
 		},
 	},
